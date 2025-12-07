5ecf67e3cf100e7742f9ab180b7a7e6cff730c48
kokokoharu <jasonleng1995@gmail.com>
2025-12-07 12:27:28 -0800

    more cleanup

--- gpu/main.cpp
+++ gpu/main.cpp
@@ -9,69 +9,6 @@
 
 using namespace std;
 
-void testComputeWeight() {
-    // load in image and invert gamma correction
-    Image im1("../Input/ante1-1.png");
-    im1 = gamma_code(im1, 1.0/2.2);
-
-    // compute the weight image and save it
-    Image weight = computeWeight(im1, 0.01, 0.99);
-    weight.write("./Output/weight.png");
-}
-
-void testComputeFactor() {
-    // load 2 images
-    Image im1("../Input/ante2-1.png");
-    Image im2("../Input/ante2-2.png");
-
-    // invert gamma correction
-    im1 = gamma_code(im1, 1.0/2.2);
-    im2 = gamma_code(im2, 1.0/2.2);
-
-    // compute weight images and save them
-    Image w1 = computeWeight(im1);
-    Image w2 = computeWeight(im2);
-    w1.write("./Output/ante2-w1.png");
-    w2.write("./Output/ante2-w2.png");
-
-    // compute the factor
-    float factor = computeFactor(im1, w1, im2, w2);
-    cout << "factor: " << factor << endl;
-}
-
-
-void testMakeHDR_GPU() {
-    // load an image sequence
-    vector<Image> imSeq;
-    imSeq.push_back(gamma_code(Image("../Input/design-1.png"), 1.0/2.2));
-    imSeq.push_back(gamma_code(Image("../Input/design-2.png"), 1.0/2.2));
-    imSeq.push_back(gamma_code(Image("../Input/design-3.png"), 1.0/2.2));
-    imSeq.push_back(gamma_code(Image("../Input/design-4.png"), 1.0/2.2));
-    imSeq.push_back(gamma_code(Image("../Input/design-5.png"), 1.0/2.2));
-    imSeq.push_back(gamma_code(Image("../Input/design-6.png"), 1.0/2.2));
-    imSeq.push_back(gamma_code(Image("../Input/design-7.png"), 1.0/2.2));
-
-    // generate an hdr image
-    Image hdr = makeHdrGpuBasic(imSeq);
-    hdr.write("./Output/hdr_image");
-
-    // save out images clipped to different ranges.
-    float maxVal = hdr.max();
-    Image hdrScale0 = gamma_code(hdr/maxVal, 2.2);
-    hdrScale0.write("./Output/scaledHDR_design_0.png");
-    Image hdrScale2 = gamma_code((2e2)*hdr/maxVal, 2.2);
-    hdrScale2.write("./Output/scaledHDR_design_2.png");
-    Image hdrScale4 = gamma_code((2e4)*hdr/maxVal, 2.2);
-    hdrScale4.write("./Output/scaledHDR_design_4.png");
-    Image hdrScale6 = gamma_code((2e6)*hdr/maxVal, 2.2);
-    hdrScale6.write("./Output/scaledHDR_design_6.png");
-    Image hdrScale8 = gamma_code((2e8)*hdr/maxVal, 2.2);
-    hdrScale8.write("./Output/scaledHDR_design_8.png");
-    Image hdrScale10 = gamma_code((2e10)*hdr/maxVal, 2.2);
-    hdrScale10.write("./Output/scaledHDR_design_10.png");
-
-}
-
 // HDR and Tone Mapping on Ante1 images
 void testToneMapping_ante1() {
 
